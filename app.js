f2edc7e7baceb0e4cba21d4abd970418d741e82e
DikshaDhalpe <Dikshadhalpe225@gmail.com>
2026-01-27 15:54:31 +0530

    Basic set up

--- app.js
+++ app.js
@@ -2,7 +2,31 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const MONGOURL = "mongodb://127.0.0.1:27017/test";
+main()
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch(() => {
+        console.log(err);
+    })
+async function main() {
+    await mongoose.connect(MONGOURL);
+
+}
+
+
+
+
+
+
+
+
 //basic setup for app
+app.get("/", (req, res) => {
+    res.send("I am  root 8080");
+});
+
 app.listen(8080, () => {
     console.log("Server is listening to port 8080");
 });
