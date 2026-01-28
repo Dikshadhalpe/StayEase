dce9087179f11a7c19f507fb3e0e378dfdc8554a
DikshaDhalpe <Dikshadhalpe225@gmail.com>
2026-01-28 19:14:50 +0530

    Navbar & Footer

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGOURL = "mongodb://127.0.0.1:27017/StayEase";
 main()
@@ -22,6 +23,8 @@ app.set("vivew engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 
 //basic setup for app
@@ -38,7 +41,6 @@ app.get("/listings", async (req, res) => {
 app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 });
-
 
 
 //show route
